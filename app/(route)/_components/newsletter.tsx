4bf2fb8a3a31325d5c3b4832d60f32c3783441b1
nauman434 <76839009+nauman434@users.noreply.github.com>
2024-04-18 14:55:57 +0500

    newsletter fixed

--- app/(route)/_components/newsletter.tsx
+++ app/(route)/_components/newsletter.tsx
@@ -9,7 +9,7 @@ const Newsletter: React.FC = () => {
 
     return (
         <Container className='mt-[50px]'>
-            <section className='rounded-lg bg-black p-[20px]' >
+            <section className='rounded-lg bg-black p-[5px]' >
                 <div className='flex py-[50px] flex-col justify-center items-center  gap-[20px] '>
                     <div className='sm:w-[540px] w-full flex flex-col gap-[20px]'>
                         <h1 className='sm:text-3xl text-xl font-semibold text-center text-white leading-[100%]'>Get the latest jobs in your inbox</h1>
